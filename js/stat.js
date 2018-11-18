0e255abbcc63cadd70913218c49759a5f7ccf4ea
Aleksandr_Kushniruk <kushniruk_aleksandr@ukr.net>
2018-11-18 19:54:44 +0200

    Отрисовывает текст сообщения перед гистограммой

--- js/stat.js
+++ js/stat.js
@@ -4,12 +4,25 @@ var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var cloudMessage = 'Ура вы победили!\nСписок результатов:';
 
+//Функция отрисовки облака
 function renderCloud (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
 window.renderStatistics = function (ctx) {
+  //Отрисовка тени под облаком
   renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
+
+  //Отрисовка облака
   renderCloud(ctx, 100, 10, '#fff');
+
+  //Отрисовка текста сообщения
+  ctx.fillStyle = '#000';
+  ctx.font = '16px PT Mono';
+  ctx.textBaseline = 'hanging';
+  cloudMessage = cloudMessage.split('\n');
+  for (var i = 0; i < cloudMessage.length; i++) {
+    ctx.fillText(cloudMessage[i], 120, 30 + 20 * i);
+  }
 };
