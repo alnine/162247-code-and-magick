296a9713e0a4f8ba56c64541e65c10df8719ca5a
Aleksandr_Kushniruk <kushniruk_aleksandr@ukr.net>
2018-11-19 13:29:25 +0200

    Устанавливает базовую линию текста по-умолчанию

--- js/stat.js
+++ js/stat.js
@@ -23,34 +23,33 @@ window.renderStatistics = function (ctx) {
   //Отрисовка текста сообщения
   ctx.fillStyle = '#000';
   ctx.font = '16px PT Mono';
-  ctx.textBaseline = 'hanging';
   cloudMessage = cloudMessage.split('\n');
   for (var i = 0; i < cloudMessage.length; i++) {
-    ctx.fillText(cloudMessage[i], 120, 30 + 20 * i);
+    ctx.fillText(cloudMessage[i], 120, 40 + 20 * i);
   }
 
   //Отрисовка гистограммы
   ctx.fillText('1000', 140, 80);
   ctx.fillStyle = 'red';
-  ctx.fillRect(140, 100, 40, 150);
+  ctx.fillRect(140, 90, 40, 150);
   ctx.fillStyle = '#000';
   ctx.fillText('Вы', 140, 260);
 
   ctx.fillText('1000', 230, 80);
   ctx.fillStyle = 'blue';
-  ctx.fillRect(230, 100, 40, 150);
+  ctx.fillRect(230, 90, 40, 150);
   ctx.fillStyle = '#000';
   ctx.fillText('Кекс', 230, 260);
 
   ctx.fillText('1000', 320, 80);
   ctx.fillStyle = 'blue';
-  ctx.fillRect(320, 100, 40, 150);
+  ctx.fillRect(320, 90, 40, 150);
   ctx.fillStyle = '#000';
   ctx.fillText('Катя', 320, 260);
 
   ctx.fillText('1000', 410, 80);
   ctx.fillStyle = 'blue';
-  ctx.fillRect(410, 100, 40, 150);
+  ctx.fillRect(410, 90, 40, 150);
   ctx.fillStyle = '#000';
   ctx.fillText('Игорь', 410, 260);
 };
