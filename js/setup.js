3ea93a50d704fce5aaae146095b7becb8cec8e60
Aleksandr_Kushniruk <kushniruk_aleksandr@ukr.net>
2018-11-22 00:46:04 +0200

    Добавляет функцию создания объекта волшебника

--- js/setup.js
+++ js/setup.js
@@ -2,11 +2,21 @@
 
 var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0'];
+var COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
 
 function getRandomElementOfArray(arr) {
   return arr[getRandomInt(0, arr.length - 1)];
+}
+
+function createWizardObject(name, surname, coatColor, eyesColor) {
+  var wizardObject = {
+    name: name + ' ' + surname,
+    coatColor: coatColor,
+    eyesColor: eyesColor
+  };
+
+  return wizardObject;
 }
 
 var userSetup = document.querySelector('.setup');
